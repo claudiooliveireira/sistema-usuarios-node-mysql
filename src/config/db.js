0cc91622aacd5485093f656c6c477f46e3a5df05
Claudio Oliveira <claudio.oliveira.sants@gmail.com>
2026-05-23 17:00:31 -0400

    Update database credentials in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'oliveira%Santo@',
-    database: 'curso_node'
+    password: 'suaSenha',
+    database: 'nomeDoDatabse'
 });
 
 connection.connect((err) => {
